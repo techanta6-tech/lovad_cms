420e4bb80b41af393f9b10b2ac24c6afb7ae386c
phucphan01866 <111278365+phucphan01866@users.noreply.github.com>
2026-07-16 01:38:35 +0700

    Up

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,3 +1,9 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+// Nạp các biến môi trường từ file .env
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
